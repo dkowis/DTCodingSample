3a8f837093bf3c1c71d2d1402f3482a5f751cd32
David Kowis <dkowis@shlrm.org>
2015-10-20 18:45:42 -0500

    Writing a failing test with just enough to compile

---
+++ src/main/java/dealertrack/KeyCounts.java
@@ -0,0 +1,12 @@
+package dealertrack;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class KeyCounts {
+
+    public static String countKeys(URL file) {
+        return "The result!";
+    }
+}
---
+++ src/test/scala/dealertrack/KeyCountsTest.scala
@@ -0,0 +1,22 @@
+package dealertrack
+
+import org.junit.runner.RunWith
+import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class KeyCountsTest extends FunSpec with Matchers{
+
+  describe("summing up keys from a file") {
+    it("sums up keys properly") {
+      //Given a uri to a file
+      val url = getClass.getResource("/keyCounts.txt")
+
+      //When I ask my function to sum it for me
+      val result = KeyCounts.countKeys(url)
+
+      //I should get back the proper results for my sample file
+      result should equal("The total for John is 6. The total for Jane is 8.")
+    }
+  }
+}
